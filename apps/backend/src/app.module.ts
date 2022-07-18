59be1e37a6ba56199c20ca259f58b48e27333ca4
naoto613 <fujii140@gmail.com>
2022-07-18 22:34:35 +0900

    test

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -14,7 +14,7 @@ import { ComplexityPlugin } from './infrastructure/plugins/complexity.plugin';
         process.cwd(),
         'apps/backend/src/infrastructure/prisma/schema.gql',
       ),
-      // corsの設定が必要な場合
+      // corsの設定が必要な場合に外す
       // cors: {
       //   origin: process.env.ORIGINS?.split(','),
       //   credentials: true,
