bc960f087a2a149afeb7da7b96cf9c38a41b69f8
naoto613 <fujii140@gmail.com>
2022-08-27 23:56:23 +0900

    bigquery

--- apps/backend/src/application/use-cases/users/find-users.use-case.ts
+++ apps/backend/src/application/use-cases/users/find-users.use-case.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { UserModel } from '../../../domain/models/user.model';
 import { SearchUsersInput } from '../../../dto/users/search-users.input';
 import { UsersRepository } from '../../repositories/users.repository';
+import * as fs from 'fs';
+import { BigQuery } from '@google-cloud/bigquery';
 
 @Injectable()
 export class FindUsersUseCase {
@@ -10,6 +12,25 @@ export class FindUsersUseCase {
   async execute(
     input: Readonly<SearchUsersInput>,
   ): Promise<UserModel[] | null> {
+    // fs.writeFileSync("./temp/gcp-key.json", process.env.GCP_KEY_FILE_TEXT)
+    
+    // const query = "select * from test.members";
+    
+    // await new BigQuery().query(query)
+    //   .then(data => {
+    //     console.log(data);
+    //   })
+    //   .catch(error => {
+    //       console.log(error);
+    //   });
+        
+    // fs.unlink("./temp/gcp-key.json", (err) => {
+    //   if (err) {
+    //     console.log('ファイルの削除に失敗しました。');  
+    //   };
+    //   console.log('ファイルを削除しました。');
+    // });
+  
     return this.repository.findMany(input);
   }
 }
