3c2e68f8b09b0f3ca0c5ca4b8d7b70ae5ba36b24
naoto613 <fujii140@gmail.com>
2022-08-27 23:55:30 +0900

    batch action

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { FindUsersUseCase } from './application/use-cases/users/find-users.use-case';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  // console.log(process.env.BATCH)
+  if (process.env.BATCH) {
+    // console.log('どうだ')
+    const findUsersUseCase = app.get(FindUsersUseCase);
+    const users = findUsersUseCase.execute({searchInput:  null})
+    console.log(users)
+    await app.close();
+  } else {
+    await app.listen(3000);
+  }
 }
 bootstrap();
+
+// async function bootstrap() {
+//   // const app = await NestFactory.create(AppModule);
+//   const app = await NestFactory.createApplicationContext(AppModule);
+//   // app.useGlobalPipes(new ValidationPipe({ transform: true }));
+//   console.log('どうだ')
+// }
+// bootstrap();
